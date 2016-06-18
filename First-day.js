5bc08ae9c6d47d0cbcb40246bb73ce0fb26c5de7
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-06-18 17:54:28 +0100

    Update First-day.js

--- First-day.js
+++ First-day.js
@@ -1,18 +1,18 @@
 // Day one
 // Angular Scope
 /* $scope is a Javascript object that owns application variable data in Angular JS. */
-	 var application = angular.module("myApp",[]);
-   // define app
-	 application.controller("Control",function($scope) {
-   // create controller function
-   /* we can pass the $scope object to the 
-	    controller function as an argument */
-		 $scope.prop = "someData";
-		 $scope.propTwo = "someMoreData";
-		 // set properties of $scope object
-  	/*  these are accessable in the view (HTML),
-		    aswell as in the controller */
-	 });
+var application = angular.module("myApp",[]);
+// define app
+  application.controller("Control",function($scope) {
+  // create controller function
+  /* we can pass the $scope object to the 
+     controller function as an argument */
+     $scope.prop = "someData";
+     $scope.propTwo = "someMoreData";
+   // set properties of $scope object
+   /* these are accessable in the view (HTML),
+      aswell as in the controller. */
+   });
 
 	 
 	 
