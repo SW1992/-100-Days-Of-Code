50d36437ab0bdbc37f09746524a37985657de279
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-07-28 18:14:42 +0100

    Update Forty-first-day.py

--- Forty-first-day.py
+++ Forty-first-day.py
@@ -5,7 +5,7 @@
 # I will cover each of these & look briefly at the contrast between them & statically typed & weakly typed 
 
 # Dynamically Typed
-# Because Python is dynamically typed, so you don't have to declare variable types before use
+# Because Python is dynamically typed, you don't have to declare variable types before use
 # infact the variable itself doesn't even have a type, only the variable value does
 # the variable is just a value bound to a name, which gives you versatility to reassign variables values of different types, as needed
 some_var = "string"
@@ -22,12 +22,12 @@ print(type(some_var))
 # Strongly Typed 
 # Python is also, strongly typed, this means that operations you perform have to be compatible with the data type/types you are performing them on
 # str = "Hi" + 55 
-# would raise a type error in python, because you cannot concatenate strings & integers
+# would raise a type error in Python, because you cannot concatenate strings & integers
 # strongly typed langauges will raise an error if you're performing an operation like this that it considers wrong
 
 # by contrast, in a weakly typed language like Javascript, this kind of operation would be allowed
 # var str = "Hi" + 55;
-# type conversion would be performed by the interpreter or compiler, converting the number to a string, 
+# type conversion would be performed by the interpreter or compiler, converting the number to a string 
 # to allow you to perform such an operation, storing a concatenated string "Hi55"
 # with weakly typed languages an unexpected type conversion could cause unexpected results
 # which would fall silent, rather than raise an error, which could lead to difficult debugging
