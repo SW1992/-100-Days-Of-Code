c40171766d3b393f00edafdc2f1048dc3c160395
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-08-13 20:20:01 +0100

    Update index.js
    Made Further Progress

--- Angular-To-Do-App/index.js
+++ Angular-To-Do-App/index.js
@@ -1,11 +1,12 @@
+// Day Fifty Seven
 var app = angular.module("toDoApp",[]);
-app.controller("toDoCtrl",["$scope",function($scope) {
-  $scope.todoarr = [ 
+app.controller("toDoCtrl",function($scope) {
+  $scope.toDoArr = [   
     {"title": "Create To Do List App", "completed": false}
-  ];    
+  ];         
   // array to hold to do list items
-  $scope.addItem = function() {} 
+  $scope.addItem = function() {}   
   // method to add items  
   $scope.clearMarked = function() {}
-  // method to clear marked items  
-}])
+  // method to clear marked items   
+});   
