98fb7c67e33a213a0f915f1f30d4bf6b961e43ae
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-08-19 19:13:36 +0100

    Finished Game
    Added variable to count number of guesses

--- Python-Guess-The-Number/script.py
+++ Python-Guess-The-Number/script.py
@@ -1,26 +1,29 @@
-# Day Sixty Two
+# Day Sixty Three
 # Guess The Number  
 import random # import module
 num = random.randrange(1,21,1) # generate random num between 1 - 20 
 guess = input("I'm thinking of a whole number between 1 & 20: ") 
-
-def check_guess(guess,num):
+ 
+def check_guess(guess,num,guess_counter):
+    guess_counter += 1 
+    # another guess
     try:
         guess = int(guess) 
         # try convert to int
     except ValueError:
         guess = input("Please enter a whole number: ")
-        check_guess(guess,num) 
-        # call recursively until int entered
-    if guess < num:
-        guess = input("Too Low, Try Again: ")
-        check_guess(guess,num)
-    elif guess > num:
-        guess = input("Too High, Try Again: ")
-        check_guess(guess,num)
-    elif guess > 20 or guess < 1:
+        check_guess(guess,num,guess_counter) 
+        # call recursively until int entered 
+    if guess > 20 or guess < 1:
         guess = input("Enter a whole number between 1 & 20: ")
-        check_guess(guess,num) 
+        check_guess(guess,num,guess_counter) 
+    elif guess < num:
+        guess = input("Too Low, Try Again: ")
+        check_guess(guess,num,guess_counter)
+    elif guess > num:
+        guess = input("Too High, Try Again: ") 
+        check_guess(guess,num,guess_counter)
     else:
-         print("Congratulations, you guessed right, the number was", num)
-check_guess(guess,num) 
+        print("Congratulations! you got the number in", guess_counter, "guesses") 
+check_guess(guess,num, 0)  
+
