f7e3c0c6f185bcb1378f17e0ba790c6dd5853822
Sam Whitman <SamWhitman@hotmail.co.uk>
2016-09-25 12:10:45 +0100

    Add files via upload

---
+++ One-hundredth-day.sql
@@ -0,0 +1,20 @@
+/* Day One Hundred */ 
+/* Learn SQL: UNIT 4 Multiple Tables: Part four */ 
+
+/* Aliases Notes: 
+•	AS keyword allows you to rename a column with an alias
+•	You are free to pick a name, but it must be in single quotes
+•	Here we’re renaming the name column from two different tables with aliases
+•	However, it only changes the name of column in the context of the result set */
+
+/* Generalization Notes:
+•	PRIMARY KEY columns are unique identifiers for column rows
+•	PRIMARY KEY values must be unique and can’t be NULL
+•	FOREIGN KEY columns contain the PRIMARY key of another table
+•	FORIEIGN KEYS useful for matching rows between tables
+•	SQL joins used to combine data from more than one table
+•	INNER JOIN combines rows, if a condition is true
+•	LEFT OUTER JOIN combines all rows from a left table
+•	If the condition isn’t true, it substitutes NULL values in the right table’s columns
+•	The AS keyword allows you to rename a column or table in a result set with an alias */
+
